d1b017cf5bb14f91c0607674e1771112298bbbaf
LongVT8 <vtlong2210@gmail.com>
2021-05-28 02:53:47 +0700

    Add function Home

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use App\Models\Category;
+use App\Models\Product;
+use App\Models\ProductType;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -31,6 +33,12 @@ class AppServiceProvider extends ServiceProvider
         view()->composer('userPage/layouts/search', function ($view) {
             $categories = Category::all();
             $view->with(['categories' => $categories]);
+        });
+        view()->composer('userPage/layouts/footer', function ($view) {
+            $productSales = Product::where([['discount','>=',70],['is_hot','=',0]])->orderby('discount','desc')->limit(3)->get();
+            $productHots = Product::where([['discount','>=',50],['is_hot','=',1]])->orderby('discount','desc')->limit(3)->get();
+            $productTypeRates = ProductType::where('total_rating','>=','4,5')->limit(3)->get();
+            $view->with(['productSales' => $productSales,'productHots' => $productHots,'productTypeRates'=> $productTypeRates]);
         });
     }
 }
