4c5148a211652d330585aae4a79134160f2ca0df
LongVT8 <vtlong2210@gmail.com>
2021-06-05 19:29:45 +0700

    add calendar part 1

--- app/Http/Controllers/admin/CalendarController.php
+++ app/Http/Controllers/admin/CalendarController.php
@@ -9,6 +9,7 @@ use App\Models\Product;
 use App\Models\ProductType;
 use App\Models\Transaction;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class CalendarController extends Controller
 {
@@ -21,21 +22,25 @@ class CalendarController extends Controller
         $productType = ProductType::where("category_id",$id)->pluck("name","id_product_type");
         return response()->json($productType);
     }
-    public function getProduct($id){
-        $product = Product::where("product_type_id",$id)->pluck("id_product");
-        return response()->json($product);
-    }
+    // public function getProduct($id){
+    //     $product = Product::where("product_type_id",$id)->pluck("id_product");
+    //     return response()->json($product);
+    // }
     
     public function getOrder($id){
-        $order = Orders::whenIn('product_id',$id)->get();
-        dd($order);
-        $data = array();
-        foreach($orderOfProduct as $val){
+        $order = DB::table('order')
+                    ->leftjoin('products', 'order.product_id', '=', 'products.id_product')
+                    ->leftjoin('product_types', 'products.product_type_id', '=', 'product_types.id_product_type')
+                    ->select('order.quantity','order.id_order','product_types.name','order.updated_at')->where('id_product_type', '=', $id)
+                    ->get();
+        foreach($order as $val){
+            $newDate = date("d-m-Y", strtotime($val->updated_at)); 
             $data[] = array(
-                'start' => $val->date_start.'T12:00:00',
-                'end' => $val->date_end.'T12:00:00',
-                'bill_id' => $val->bill_id,
-                'title' => "Đơn hàng số:  ".$val->bill_id,
+                'start' => $newDate.'00:00:00',
+                'end' => $newDate.'23:59:59',
+                'order_id' => $val->id_order,
+                'title' => "Đơn hàng số:  ".$val->id_order,
+                'product' => "Product :" .$val->name,
             );
         }
         return response()->json($data);
