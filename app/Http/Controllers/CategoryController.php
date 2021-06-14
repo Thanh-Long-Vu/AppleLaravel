19c938f74b607781986d917edd202e16169beacd
Thanh Long <vtlong2210@gmail.com>
2021-06-15 00:39:50 +0700

    update page categories

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
+use App\Models\ProductType;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
@@ -10,8 +11,11 @@ class CategoryController extends Controller
     public function show(Request $request, Category $category)
     {
         $categoryName = $category->name;
-        $productTypes = $category->productTypes;
-
-        return view('userPage.pages.category.categories', compact('categoryName','productTypes'));
+        $categoryId = $category->id_category;
+        $productTypes = ProductType::where('category_id',$category->id_category)->orderby('updated_at','asc')->paginate(8);
+        return view('userPage.pages.category.categories', compact('categoryName','productTypes','categoryId'));
     }
+//    public function filterAll(Request $request){
+//        dd($request->all());
+//    }
 }
